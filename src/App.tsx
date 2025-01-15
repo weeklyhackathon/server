baed0e84f1ec094d862837a8bc40ddce66765eab
Moon Man <moon@shipoclu.com>
2025-01-15 06:50:28 -0500

    handles not enough tokens now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import '@farcaster/auth-kit/styles.css';
-import { AuthKitProvider, UseSignInData, SignInButton } from '@farcaster/auth-kit';
 import {
-  Chat,
-  Channel,
-  MessageList,
-  MessageInput
-} from "stream-chat-react";
-import "stream-chat-react/dist/css/v2/index.css";
+  AuthKitProvider,
+  UseSignInData,
+  SignInButton,
+} from '@farcaster/auth-kit';
+import { Chat, Channel, MessageList, MessageInput } from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
 import { useState } from 'react';
 import './App.css';
 import { useStore } from './store/useStore';
-import { DefaultGenerics, StreamChat, type Channel as StreamChannel } from "stream-chat";
-
+import {
+  DefaultGenerics,
+  StreamChat,
+  type Channel as StreamChannel,
+} from 'stream-chat';
 
 interface LoginResponse {
+  success: boolean;
+  message: string;
   payload?: {
     username: string;
     farcasterUsername?: string;
@@ -21,7 +25,7 @@ interface LoginResponse {
     pfp: string;
     addresses: `0x${string}`[];
     powerBadge: boolean;
-  }
+  };
 }
 
 const optimismConfig = {
@@ -37,71 +41,130 @@ const chatClient = StreamChat.getInstance(apiKey, {
 });
 
 function App() {
+  const {
+    name: username,
+    setName: setUsername,
+    jwt: jwt,
+    setJwt: setJwt,
+    pfp: pfp,
+    setPfp: setPfp,
+    displayName,
+    setDisplayName,
+    clearData,
+  } = useStore();
+  const [channel, setChannel] = useState<StreamChannel<DefaultGenerics> | null>(
+    null
+  );
+  const [errors, setErrors] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  const { name: username, setName: setUsername, jwt: jwt, setJwt: setJwt, pfp: pfp, setPfp: setPfp, displayName, setDisplayName } = useStore();
-  const [channel, setChannel] = useState<StreamChannel<DefaultGenerics> | null>(null);
-
-  const nativeLogin = async (nonce: string, message: string, signature: string, fid: number, username: string) => {
+  const nativeLogin = async (
+    nonce: string,
+    message: string,
+    signature: string,
+    fid: number,
+    username: string
+  ) => {
     const response = await fetch('/api/login', {
       method: 'POST',
-      body: JSON.stringify({ fid, username, signature, nonce, message, domain: window.location.host }),
+      body: JSON.stringify({
+        fid,
+        username,
+        signature,
+        nonce,
+        message,
+        domain: window.location.host,
+      }),
       headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        Accept: 'application/json',
       },
     });
-    const data = await response.json() as LoginResponse;
-    console.log("Login response: ", data);
+    const data = (await response.json()) as LoginResponse;
+    console.log('Login response: ', data);
 
-    if (data.payload) {
-      return data.payload;
+    if (data.success) {
+      if (data.payload) {
+        return data.payload;
+      } else return 'Missing payload';
     }
 
-    return null;
-  }
+    console.error('Login failed: ', data.message);
+    return data.message;
+  };
+
+  const logoutCleanup = () => {
+    clearData();
+    setChannel(null);
+    setIsAuthenticated(false);
+  };
 
   const onFarcasterSignIn = (data: UseSignInData) => {
-    console.log("User signed in: ", data);
-    if (!data.nonce || !data.message || !data.signature || !data.fid || !data.username) {
-      console.error("Invalid sign in data");
+    console.log('User signed in: ', data);
+    if (
+      !data.nonce ||
+      !data.message ||
+      !data.signature ||
+      !data.fid ||
+      !data.username
+    ) {
+      console.error('Invalid sign in data');
+      setErrors('Invalid sign in data');
+      logoutCleanup();
       return;
     }
 
-    nativeLogin(data.nonce, data.message, data.signature, data.fid, data.username).then((userData) => {
+    nativeLogin(
+      data.nonce,
+      data.message,
+      data.signature,
+      data.fid,
+      data.username
+    ).then((userData) => {
+      if (typeof userData === 'string') {
+        console.error('Login failed: ', userData);
+        setErrors(userData);
+        logoutCleanup();
+        return;
+      }
 
-      if (userData) {
-        const { username, pfp, jwt } = userData;
-        setUsername(username);
-        if (userData.farcasterUsername) {
-          setDisplayName(userData.farcasterUsername);
-        }
-        setPfp(pfp);
-        setJwt(jwt);
+      const { username, pfp, jwt } = userData;
+      setUsername(username);
+      if (userData.farcasterUsername) {
+        setDisplayName(userData.farcasterUsername);
+      }
+      setPfp(pfp);
+      setJwt(jwt);
 
-        const userRequestData: Record<string, string> = {
-          id: username as string,
-          name: (displayName || username) as string,
-        };
-        if (pfp) {
-          userRequestData.image = pfp;
-        }
+      const userRequestData: Record<string, string> = {
+        id: username as string,
+        name: (displayName || username) as string,
+      };
+      if (pfp) {
+        userRequestData.image = pfp;
+      }
 
-        chatClient.connectUser(
-          userRequestData as any,
-          jwt,
-        ).then(() => {
-          console.log("User connected, connecting to chat channel");
-          const channel = chatClient.getChannelById("livestream", "messaging", {});
+      chatClient
+        .connectUser(userRequestData as any, jwt)
+        .then(() => {
+          console.log('User connected, connecting to chat channel');
+          const channel = chatClient.getChannelById(
+            'livestream',
+            'messaging',
+            {}
+          );
           setChannel(channel);
+          setIsAuthenticated(true);
         })
         .catch((error) => {
-          console.error("Error connecting user: ", error);
+          console.error('Error connecting user: ', error);
+          setErrors(error.message);
+          logoutCleanup();
         });
-      }
     });
   };
 
-  const StreamChat = () => {
+  const LivestreamChat = () => {
     if (!channel) {
       return null;
     }
@@ -119,36 +182,49 @@ function App() {
     );
   };
 
+  const MaybeDisplaySignInButton = () => {
+    if (jwt || isAuthenticated) {
+      return null;
+    }
+    return <SignInButton onSuccess={onFarcasterSignIn} />;
+  };
+
   if (jwt) {
-    const userData:Record<string, string> = {
+    const userData: Record<string, string> = {
       id: username,
       name: displayName || username,
     };
     if (pfp) {
       userData.image = pfp;
     }
-    chatClient.connectUser(
-      userData as any,
-      jwt,
-    ).then(() => {
-      console.log("User connected, connecting to chat channel");
-      const channel = chatClient.getChannelById("livestream", "messaging", {});
-      setChannel(channel);
-    })
-    .catch((error) => {
-      console.error("Error connecting user: ", error);
-    });
+    chatClient
+      .connectUser(userData as any, jwt)
+      .then(() => {
+        console.log('User connected, connecting to chat channel');
+        const channel = chatClient.getChannelById(
+          'livestream',
+          'messaging',
+          {}
+        );
+        setChannel(channel);
+        setIsAuthenticated(true);
+      })
+      .catch((error) => {
+        console.error('Error connecting user: ', error);
+        setErrors(error.message);
+      });
   }
 
-    return (
-      <AuthKitProvider config={optimismConfig}>
+  return (
+    <AuthKitProvider config={optimismConfig}>
       <h1>Weeklyhackathon</h1>
+      {errors && <p className="error">{errors}</p>}
       {pfp && <img src={pfp} alt="Profile picture" />}
-      <StreamChat />
+      <LivestreamChat />
       <div>
-      {!jwt && <SignInButton onSuccess={onFarcasterSignIn} />  /* TODO handle the JWT expiring */ }
+        <MaybeDisplaySignInButton />
       </div>
-   </AuthKitProvider>
+    </AuthKitProvider>
   );
 }
 
--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -2,39 +2,50 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 export interface PersistentStore {
-    name: string;
-    setName: (newName: string) => void;
-    jwt: string;
-    token: string;
-    setJwt: (newJwt: string) => void;
-    setToken: (newToken: string) => void;
-    version: number;
-    setVersion: (newVersion: number) => void;
-    pfp: string;
-    setPfp: (newPfp: string) => void;
-    displayName: string;
-    setDisplayName: (newDisplayName: string) => void;
+  name: string;
+  setName: (newName: string) => void;
+  jwt: string;
+  token: string;
+  setJwt: (newJwt: string) => void;
+  setToken: (newToken: string) => void;
+  version: number;
+  setVersion: (newVersion: number) => void;
+  pfp: string;
+  setPfp: (newPfp: string) => void;
+  displayName: string;
+  setDisplayName: (newDisplayName: string) => void;
+  clearData: () => void;
 }
 
 // Create a store with persistence
 export const useStore = create<PersistentStore>()(
-    persist(
-        (set) => ({
-            name: '',
-            setName: (newName: string) => set({ name: newName }),
-            jwt: '',
-            token: '',
-            setJwt: (newJwt: string) => set({ jwt: newJwt }),
-            setToken: (newToken: string) => set({ token: newToken }),
-            version: 0,
-            setVersion: (newVersion: number) => set({ version: newVersion }),
-            pfp: '',
-            setPfp: (newPfp: string) => set({ pfp: newPfp }),
-            displayName: '',
-            setDisplayName: (newDisplayName: string) => set({ displayName: newDisplayName }),
-        }),
-        {
-            name: 'user-storage',
-        }
-    )
+  persist(
+    (set) => ({
+      name: '',
+      setName: (newName: string) => set({ name: newName }),
+      jwt: '',
+      token: '',
+      setJwt: (newJwt: string) => set({ jwt: newJwt }),
+      setToken: (newToken: string) => set({ token: newToken }),
+      version: 0,
+      setVersion: (newVersion: number) => set({ version: newVersion }),
+      pfp: '',
+      setPfp: (newPfp: string) => set({ pfp: newPfp }),
+      displayName: '',
+      setDisplayName: (newDisplayName: string) =>
+        set({ displayName: newDisplayName }),
+      clearData: () =>
+        set({
+          name: '',
+          jwt: '',
+          token: '',
+          pfp: '',
+          displayName: '',
+          version: 0,
+        }), // Fix version later, it should not be reset.
+    }),
+    {
+      name: 'user-storage',
+    }
+  )
 );
