e18b84c256634c2246b136798f8068ea9108e609
Moon Man <moon@shipoclu.com>
2025-01-15 11:56:21 -0500

    fix remove privy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import 'stream-chat-react/dist/css/v2/index.css';
 import { useState } from 'react';
 import './App.css';
 import { useStore } from './store/useStore';
-import MaybeDisplaySignInButton, { PrivyOnCompleteParams} from './components/MaybeDisplaySignInButton/MaybeDisplaySignInButton';
+import MaybeDisplaySignInButton from './components/MaybeDisplaySignInButton/MaybeDisplaySignInButton';
 import {
   DefaultGenerics,
   StreamChat,
@@ -129,22 +129,6 @@ function App() {
     setIsAuthenticated(false);
   };
 
-  const onPrivySignIn = (data: PrivyOnCompleteParams) => {
-    console.log('Privy user signed in: ', data);
-    const wallet = data.user.wallet?.address;
-    if (!wallet) {
-      console.error('No wallet address found');
-      logoutCleanup();
-      return;
-    }
-
-    console.log('Was already authenticated? ', data.wasAlreadyAuthenticated);
-  };
-
-  const onPrivyError = (error: any) => {
-    console.log('Privy login error: ', error);
-  };
-
   const onFarcasterSignIn = (data: UseSignInData) => {
     console.log('User signed in: ', data);
     if (
@@ -253,7 +237,7 @@ function App() {
       {pfp && <img src={pfp} alt="Profile picture" />}
       <LivestreamChat channel={channel} displayName={displayName} username={username} />
       <div>
-        <MaybeDisplaySignInButton jwt={jwt} isAuthenticated={isAuthenticated} onFarcasterSignIn={onFarcasterSignIn} onPrivySignIn={onPrivySignIn} onPrivyError={onPrivyError} />
+        <MaybeDisplaySignInButton jwt={jwt} isAuthenticated={isAuthenticated} onFarcasterSignIn={onFarcasterSignIn} />
       </div>
       </PrivyProvider>
     </AuthKitProvider>
--- src/components/MaybeDisplaySignInButton/MaybeDisplaySignInButton.tsx
+++ src/components/MaybeDisplaySignInButton/MaybeDisplaySignInButton.tsx
@@ -2,32 +2,15 @@ import {
     SignInButton,
     UseSignInData,
 } from '@farcaster/auth-kit';
-import { useLogin as usePrivyLogin, User } from '@privy-io/react-auth';
 
-export interface PrivyOnCompleteParams {
-    user: User;
-    isNewUser: boolean;
-    wasAlreadyAuthenticated: boolean;
-}
-
-const MaybeDisplaySignInButton = ({ jwt, isAuthenticated, onFarcasterSignIn, onPrivySignIn, onPrivyError }: { jwt: string, isAuthenticated: boolean, onFarcasterSignIn: (data: UseSignInData) => void, onPrivySignIn: (data: PrivyOnCompleteParams) => void, onPrivyError: (error: any) => void }) => {
+const MaybeDisplaySignInButton = ({ jwt, isAuthenticated, onFarcasterSignIn }: { jwt: string, isAuthenticated: boolean, onFarcasterSignIn: (data: UseSignInData) => void }) => {
     if (jwt || isAuthenticated) {
         return null;
     }
 
-    // onComplete can fire when page is loaded.
-    const { login: privyLogin } = usePrivyLogin({
-        onComplete: onPrivySignIn,
-        onError: onPrivyError,
-    });
-
     return (
         <div>
             <SignInButton onSuccess={onFarcasterSignIn} />
-
-            <button onClick={privyLogin}>
-                Login with Wallet
-            </button>
         </div>
     );
 };
